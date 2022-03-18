a49ab76dd56dd8ff39c1af5f0fba113666094e45
Zachary Johnson <zachjohnson543@gmail.com>
2022-03-18 12:09:47 -0700

    Added title file, designed button class

---
+++ Button.py
@@ -0,0 +1,13 @@
+import App
+
+class Button():
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+    def update(self):
+        pass
+    def click(self):
+        pass
+    def hover(self):
+        pass
---
+++ TitleScreen.py
@@ -0,0 +1,9 @@
+from socket import TIPC_CLUSTER_SCOPE
+from tokenize import Whitespace
+from App import screen
+import Button
+
+def draw():
+    pass
+def update(): #redraw buttons for when they are hovered or unhovered
+    pass
