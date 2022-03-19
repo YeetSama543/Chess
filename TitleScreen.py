154e5f6f50c6a628d5c3c2a60151d2fea6f785e1
Zachary Johnson <zachjohnson543@gmail.com>
2022-03-19 15:48:04 -0700

    created buttons for title screen

--- TitleScreen.py
+++ TitleScreen.py
@@ -1,9 +1,15 @@
-from socket import TIPC_CLUSTER_SCOPE
-from tokenize import Whitespace
+import pygame
+from Game_Constants import *
 from App import screen
-import Button
+from Button import Button
+
+choice1 = Button(TITLE_BUTTON_WIDTH, TITLE_BUTTON_HEIGHT, BUTTON_TEXT_COLOR, BUTTON_BG_COLOR, BUTTON_HOVER_COLOR, False, False, "Human Vs Human")
+choice1 = Button(TITLE_BUTTON_WIDTH, TITLE_BUTTON_HEIGHT, BUTTON_TEXT_COLOR, BUTTON_BG_COLOR, BUTTON_HOVER_COLOR, False, False, "Human Vs Computer")
+choice1 = Button(TITLE_BUTTON_WIDTH, TITLE_BUTTON_HEIGHT, BUTTON_TEXT_COLOR, BUTTON_BG_COLOR, BUTTON_HOVER_COLOR, False, False, "Quit")
 
 def draw():
-    pass
+    title_text_box = pygame.font.SysFont("arial", 50).render("Chess", True, BUTTON_TEXT_COLOR)
+    screen.fill(TITLE_BG_COLOR)
+
 def update(): #redraw buttons for when they are hovered or unhovered
     pass
