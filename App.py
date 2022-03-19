1885fecb7644d31d9672289e52a1c8685ebc4fd9
Zachary Johnson <zachjohnson543@gmail.com>
2022-03-19 15:46:51 -0700

    fill screen to white when initialized

--- App.py
+++ App.py
@@ -5,5 +5,6 @@ from Game_Constants import *
 
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
+screen.fill(WHITE)
 font = pygame.font.SysFont("arial", 20)
-clock = pygame.time.Clock
+clock = pygame.time.Clock()
