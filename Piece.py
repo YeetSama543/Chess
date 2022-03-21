a7691c32aa8f19cd6513b280862a3db7d1748ef0
Zachary Johnson <zachjohnson543@gmail.com>
2022-03-21 15:21:21 -0700

    Created PieceType enum

---
+++ Piece.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class PieceType(Enum):
+    WHITE_PAWN = 'P'
+    WHITE_KNIGHT = 'N'
+    WHITE_BISHOP = 'B'
+    WHITE_ROOK = 'R'
+    WHITE_QUEEN = 'Q'
+    WHITE_KING = 'K'
+
+    BLACK_PAWN = 'p'
+    BLACK_KNIGHT = 'n'
+    BLACK_BISHOP = 'b'
+    BLACK_ROOK = 'r'
+    BLACK_QUEEN = 'q'
+    BLACK_KING = 'k'
+    
