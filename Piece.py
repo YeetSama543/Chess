f88fddc118eb416f7e847f11663ab4d65259b21f
Zachary Johnson <zachjohnson543@gmail.com>
2022-03-21 15:45:42 -0700

    created skeleton Piece class, added color enum

--- Piece.py
+++ Piece.py
@@ -1,6 +1,7 @@
+import pygame as pg
 from enum import Enum
 
-class PieceType(Enum):
+class Type(Enum):
     WHITE_PAWN = 'P'
     WHITE_KNIGHT = 'N'
     WHITE_BISHOP = 'B'
@@ -14,4 +15,19 @@ class PieceType(Enum):
     BLACK_ROOK = 'r'
     BLACK_QUEEN = 'q'
     BLACK_KING = 'k'
-    
+
+class Color(Enum):
+    WHITE = 0
+    BLACK = 1
+
+class Piece:
+    def __init__(self, color: Color, type: Type, square: list):
+        self.color = color
+        self.type = type
+        self.square = square
+    def draw(self, surf: pg.Surface, pos: tuple):
+        pass
+    def place(self, newSquare: list):
+        self.square = newSquare
+    def remove(self):
+        self.square = None
