cd21ffe4b6b185fd7fa57b24a7215e49a00b82c1
Zachary Johnson <zachjohnson543@gmail.com>
2022-03-21 15:42:50 -0700

    imported pygame as pg

--- TitleScreen.py
+++ TitleScreen.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame as pg
 from Game_Constants import *
 from App import screen
 from Button import Button
@@ -9,7 +9,7 @@ choice2 = Button(TITLE_BUTTON_WIDTH, TITLE_BUTTON_HEIGHT, BUTTON_TEXT_COLOR, BUT
 choice3 = Button(TITLE_BUTTON_WIDTH, TITLE_BUTTON_HEIGHT, BUTTON_TEXT_COLOR, BUTTON_BG_COLOR, BUTTON_HOVER_COLOR, False, False, "Quit")
 buttons = [choice1, choice2, choice3]
 #positioning vars
-title_text_box = pygame.font.SysFont("arial", 50).render("Chess", True, BUTTON_TEXT_COLOR)
+title_text_box = pg.font.SysFont("arial", 50).render("Chess", True, BUTTON_TEXT_COLOR)
 title_width = title_text_box.get_width()
 title_height = title_text_box.get_height()
 title_x = (SCREEN_SIZE[0] - title_width) // 2
