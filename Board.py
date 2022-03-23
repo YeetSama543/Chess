6f9a47fe6d7d18abf478232892cc6406fc298836
Zachary Johnson <zachjohnson543@gmail.com>
2022-03-23 10:41:12 -0700

    worked on draw, fixed bug in add pieces

--- Board.py
+++ Board.py
@@ -1,7 +1,6 @@
 import pygame as pg
 from Piece import *
 from Game_Constants import *
-from App import screen
 
 class Board:
     def __init__(self, pos: tuple):
@@ -9,12 +8,12 @@ class Board:
         self.position = [[None for i in range(8)] for j in range(8)]
         self.pieces = []
         self.clicked_piece = None
-        
+
     def add_pieces(self): #adds all pieces onto board for start of game
         #rooks
-        self.pieces.append(Piece(Type.WHITE_ROOK, [0,7]))
+        self.pieces.append(Piece(Type.WHITE_ROOK, [7,0]))
         self.pieces.append(Piece(Type.WHITE_ROOK, [7,7]))
-        self.pieces.appemd(Piece(Type.BLACK_ROOK, [0,0]))
+        self.pieces.append(Piece(Type.BLACK_ROOK, [0,0]))
         self.pieces.append(Piece(Type.BLACK_ROOK, [0,7]))
 
         #knights
@@ -45,7 +44,27 @@ class Board:
         for piece in self.pieces:
             self.position[piece.square[0]][piece.square[1]] = piece
 
+    def square_to_topleft(self, square: list) -> tuple: #returns topleft position of corresponding square
+        x = self.pos[0] + (square[1] * SQUARE_SIZE)
+        y = self.pos[1] + (square[0] * SQUARE_SIZE)
+        return (x,y)
     def draw(self, surf: pg.Surface):
-        pass
+        #create light and dark squares
+        light_square = pg.Surface((SQUARE_SIZE, SQUARE_SIZE))
+        light_square.fill(LIGHT_SQUARE_COLOR)
+        dark_square = pg.Surface((SQUARE_SIZE, SQUARE_SIZE))
+        dark_square.fill(DARK_SQUARE_COLOR)
+
+        #draw squares
+        for i in range(8):
+            for j in range(8):
+                topleft = self.square_to_topleft([i,j])
+                if (i + j) % 2 == 0:
+                    surf.blit(light_square, topleft)
+                else:
+                    surf.blit(dark_square, topleft)
+
+        #draw pieces
+
     def move(self, new_square):
         pass
