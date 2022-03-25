d16d6bbcd770aabf6b109ee8999b7da321bfa265
Zachary Johnson <zachjohnson543@gmail.com>
2022-03-24 22:02:16 -0700

    fixed mistake in get_clicked_square

--- Game.py
+++ Game.py
@@ -7,7 +7,7 @@ from Game_Constants import *
 def get_clicked_square(mouse_pos: tuple, board: Board) -> list:
     relative_x = mouse_pos[0] - board.pos[0]
     relative_y = mouse_pos[1] - board.pos[1]
-    return (relative_y // SQUARE_SIZE, relative_x // SQUARE_SIZE) #x and y get flipped since first val represents the row# and second is col#
+    return [relative_y // SQUARE_SIZE, relative_x // SQUARE_SIZE] #x and y get flipped since first val represents the row# and second is col#
 
 def clicked_on_board(mouse_pos: tuple, board: Board) -> bool:
     min_x = board.pos[0]
