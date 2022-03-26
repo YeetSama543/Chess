c734f98876de2125335f5b18dfb0ffdbb5cac288
Zachary Johnson <zachjohnson543@gmail.com>
2022-03-26 16:59:32 -0700

    draft of __generate_attacked_square_pawn written

--- Game.py
+++ Game.py
@@ -20,8 +20,46 @@ def clicked_on_board(mouse_pos: tuple, board: Board) -> bool:
             return True
     return False
 
-def __generate_attacked_squares_pawn(pawn: Piece):
-    pass
+def is_valid_square(square: list) -> bool:
+    if square[0] > 7 or square[0] < 0 or square[1] > 7 or square[1] < 0:
+        return False
+    return True
+
+def __generate_attacked_squares_pawn(pawn: Piece, board: Board):
+    attacked_squares = []
+    #determine if the pawn has previously moved
+    has_moved = False
+    if pawn.get_color() == Color.WHITE and pawn.square[0] == 6:
+        has_moved = True
+    elif pawn.get_color() == Color.BLACK and pawn.square[0] == 1:
+        has_moved = True
+
+    #use color to determine candidate squares to add
+    if pawn.get_color() == Color.WHITE:
+        square_in_front = [pawn.square[0] - 1, pawn.square[1]]
+        square_two_ahead = [pawn.square[0] - 2, pawn.square[1]]
+        diag_left = [pawn.square[0] - 1, pawn.square[1] - 1]
+        diag_right = [pawn.square[0] - 1, pawn.square[1] + 1]
+    else: #pawn is black
+        square_in_front = [pawn.square[0] + 1, pawn.square[1]]
+        square_two_ahead = [pawn.square[0] + 2, pawn.square[1]]
+        diag_left = [pawn.square[0] + 1, pawn.square[1] - 1]
+        diag_right = [pawn.square[0] + 1, pawn.square[1] + 1]
+
+    #check if pawn can move forward
+    if is_valid_square(square_in_front) and not board.get_piece_on_square(square_in_front):
+        attacked_squares.append(square_in_front)
+        #now check if pawn can move forward twice
+        if not has_moved and is_valid_square(square_two_ahead) and not board.get_piece_on_square(square_two_ahead):
+            attacked_squares.append(square_two_ahead)
+    #check if pawn can move diagonally
+    if is_valid_square(diag_left) and board.get_piece_on_square(diag_left):
+        if board.get_piece_on_square(diag_left).get_color() != pawn.get_color():
+            attacked_squares.append(diag_left)
+    if is_valid_square(diag_right) and board.get_piece_on_square(diag_right):
+        if board.get_piece_on_square(diag_right).get_color() != pawn.get_color():
+            attacked_squares.append(diag_right)
+
 def __generate_attacked_squares_knight(knight: Piece):
     pass
 def __generate_attacked_squares_rook(rook: Piece):
