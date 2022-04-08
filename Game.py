9fd1af581b2d7efec19dba0d523427daf8c7e709
Zachary Johnson <zachjohnson543@gmail.com>
2022-04-08 09:11:37 -0700

    fixed index out of bounds error

--- Game.py
+++ Game.py
@@ -390,38 +390,41 @@ def promote(board: Board, square: list):
 
 def ep(board: Board):
     global turn
-    last_move = board.moves[-1]
+    if board.moves != []:
+        last_move = board.moves[-1]
 
-    if turn == 0: #white to move, look for black pawn moves
-        if last_move[0] == Type.BLACK_PAWN and last_move[1][0] == 3: #last pawn move is a candidate
-            for move in reversed(board.moves[:-1]):
-                if move[0] == Type.BLACK_PAWN and move[1][1] == last_move[1][1]: #same black pawn that last moved
-                    return None
+        if turn == 0: #white to move, look for black pawn moves
+            if last_move[0] == Type.BLACK_PAWN and last_move[1][0] == 3: #last pawn move is a candidate
+                for move in reversed(board.moves[:-1]):
+                    if move[0] == Type.BLACK_PAWN and move[1][1] == last_move[1][1]: #same black pawn that last moved
+                        return None
 
-            #pawn never moved before last turn, but is on a double moved square. Thus
-            #pawn must have just double moved. so we construct the ep square and return
-            ep_square = []
-            ep_square.append(last_move[1][0] - 1)
-            ep_square.append(last_move[1][1])
-            
-            return ep_square
+                #pawn never moved before last turn, but is on a double moved square. Thus
+                #pawn must have just double moved. so we construct the ep square and return
+                ep_square = []
+                ep_square.append(last_move[1][0] - 1)
+                ep_square.append(last_move[1][1])
+                
+                return ep_square
 
-        else: #no possible ep
-            return None
-    else: #black to move, look for white pawn moves
-        if last_move[0] == Type.WHITE_PAWN and last_move[1][0] == 4: #last pawn move is a candidate
-            for move in reversed(board.moves[:-1]):
-                if move[0] == Type.WHITE_PAWN and move[1][1] == last_move[1][1]: #same white pawn that last moved
-                    return None
+            else: #no possible ep
+                return None
+        else: #black to move, look for white pawn moves
+            if last_move[0] == Type.WHITE_PAWN and last_move[1][0] == 4: #last pawn move is a candidate
+                for move in reversed(board.moves[:-1]):
+                    if move[0] == Type.WHITE_PAWN and move[1][1] == last_move[1][1]: #same white pawn that last moved
+                        return None
 
-            #pawn never moved before last turn, but is on a double moved square. Thus
-            #pawn must have just double moved. so we construct the ep square and return
-            ep_square = []
-            ep_square.append(last_move[1][0] + 1)
-            ep_square.append(last_move[1][1])
-            
-            return ep_square
+                #pawn never moved before last turn, but is on a double moved square. Thus
+                #pawn must have just double moved. so we construct the ep square and return
+                ep_square = []
+                ep_square.append(last_move[1][0] + 1)
+                ep_square.append(last_move[1][1])
+                
+                return ep_square
 
-        else: #no possible ep
-            return None
+            else: #no possible ep
+                return None
+    else:
+        return None
     
